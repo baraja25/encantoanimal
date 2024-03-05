4aadbe02ad7c87d6a3114ca18147ca3136acc5ca
Jose Antonio <92746513+baraja25@users.noreply.github.com>
2024-03-05 12:52:02 +0100

    añadido transicion de imagenes en la portada.

--- recursos/js/index.js
+++ recursos/js/index.js
@@ -25,7 +25,37 @@ $(document).ready(function(){
   $('.testimonial-item').slick({
     autoplay: true, // Activar el autoplay del slider
     autoplaySpeed: 5000, // Cambiar el testimonio cada 5 segundos
-    dots: true, // Mostrar navegación de puntos
+    dots: false, // Mostrar navegación de puntos
     arrows: false // Ocultar botones de navegación
   });
 });
+
+// Lista de imágenes de fondo
+const backgrounds = [
+  'url("../recursos/portada1.jpg")',
+  'url("../recursos/portada2.jpg")',
+  'url("../recursos/portada3.jpg")',
+  'url("../recursos/portada4.jpg")',
+  'url("../recursos/portada5.jpg")',
+  'url("../recursos/portada6.jpg")'
+];
+
+// Función para cambiar la imagen de fondo
+function changeImage() {
+  const header = document.getElementById('inicio');
+  const index = Math.floor(Math.random() * backgrounds.length); // Selecciona un índice aleatorio
+  header.style.backgroundImage = backgrounds[index];
+}
+
+
+// Cambia la imagen de fondo inicial al cargar la página
+window.onload = function() {
+  changeImage();
+};
+
+
+
+// Cambia la imagen de fondo cada cierto tiempo (por ejemplo, cada 5 segundos)
+setInterval(function() {
+  changeImage();
+}, 7000);
